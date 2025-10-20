f22d7898fd82e1599c165442d076ebb8e4fd2d0d
Abdelillah-CHADLI <mohamed.abdelillah.chadli@ensia.edu.dz>
2025-10-20 20:55:33 +0100

    Added file upload endpoint with Multer validation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // Importing required packages
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
 require("dotenv").config();
 
 // Creating express application
@@ -9,9 +10,70 @@ const app = express();
 // Getting port from environment variables or use 3001 if not specified
 const PORT = process.env.PORT || 3001;
 
+// Creating storage engine, files will be stored in RAM as buffer objects, assigned it to storage variable
+const storage = multer.memoryStorage();
+
+// Setting up file upload limits
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10MB (reasonable size limit fo CV files)
+  },
+  // Adding file type filtering
+  fileFilter: (req, file, cb) => {
+    // Allowed file types
+    const allowedTypes = [
+      "image/png",
+      "image/jpeg",
+      "image/jpg",
+      "application/pdf",
+    ];
+    if (allowedTypes.includes(file.mimetype)) {
+      // mimetype is file format identifier
+      cb(null, true); // accept file
+    } else {
+      cb(new Error("Inavlid file type. Only pdf, png and jpg are allowed")); // reject file
+    }
+  },
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// File upload endpoint
+app.post("/api/upload", upload.single("cv"), (req, res) => {
+  try {
+    // Checking if file was uploaded
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        error: "No file uploaded",
+      });
+    }
+    console.log("File recieved:");
+    console.log("Name: ", req.file.originalname);
+    console.log("Size: ", req.file.size, " bytes");
+    console.log("Type: ", req.file.mimetype);
+
+    // Send success response
+    res.json({
+      success: true,
+      message: "File uploaded successfully",
+      file: {
+        name: req.file.originalname,
+        size: req.file.size,
+        type: req.file.mimetype,
+      },
+    });
+  } catch (error) {
+    console.error("Upload error: ", error);
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
 // Routes
 app.get("/api/health", (req, res) => {
@@ -39,6 +101,30 @@ app.get("/api/error", (req, res) => {
     success: false,
     error: "This is a test error",
   });
+});
+
+// Multer Error Handling
+app.use((error, req, res, next) => {
+  if (error instanceof multer.MulterError) {
+    // Multer specific errors
+    if (error.code == "LIMIT_FILE_SIZE") {
+      return res.status(400).json({
+        success: false,
+        error: "File size exceeds 10MB limit",
+      });
+    }
+    return res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  } else if (error) {
+    // Other errors
+    return res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+  next();
 });
 
 // Start server
